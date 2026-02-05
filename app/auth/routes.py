61331997324826ef022f197356545bcfcd5f8815
Bla005-1 <blasupercell@gmail.com>
2026-02-05 16:00:19 -0500

    add simple register and login functionality

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 from app.auth import auth_bp
+from app.auth.services import login_user, register_user
 
 @auth_bp.route('/')  # probably should put this route somewhere else
 def home():
@@ -9,11 +10,29 @@ def home():
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        pass  # add form handling here
+        username = request.form['username']
+        password = request.form['password']
+
+        success = login_user(username, password)
+
+        if success:
+            return redirect(url_for('main.about'))
+
+        return redirect(url_for('auth.login'))
+
     return render_template('login.html')
 
-@auth_bp.route('/register')
+@auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        role = request.form['role']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+
+        register_user(username, password, role, email)
+        return redirect(url_for('auth.login'))
+
     return render_template('register.html')
 
 @auth_bp.route('/about')  # this route may not belong here either, just for organization
---
+++ app/auth/services.py
@@ -0,0 +1,24 @@
+# Business logic for the auth routes
+from app.models.users import User, RoleType
+from app.extensions import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+def login_user(username, password):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return False
+    return check_password_hash(user.password, password)
+
+
+def register_user(username: str, password: str, role: str, email: str):
+    role = role.upper()
+    password_hash = generate_password_hash(password)
+    user = User(
+        username=username,
+        password=password_hash,
+        role_type=RoleType[role],
+        email=email,
+    )
+    db.session.add(user)
+    db.session.commit()
