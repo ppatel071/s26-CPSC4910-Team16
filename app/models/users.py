a7309d127541e8e15daae5306c6129d431527674
Bla005-1 <blasupercell@gmail.com>
2026-02-12 09:57:02 -0500

    changes to make type checkers happy

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,8 @@
 from app.extensions import db
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, Enum
 from flask_login import UserMixin
-from datetime import datetime
+import datetime as dt
 import enum
 
 
@@ -13,12 +15,15 @@ class RoleType(enum.Enum):
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True)
-    create_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    role_type = db.Column(db.Enum(RoleType), nullable=False)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str | None] = mapped_column(String(255), unique=True)
+    create_time: Mapped[dt.datetime] = mapped_column(DateTime, default=dt.datetime.now(dt.timezone.utc), nullable=False)
+    role_type: Mapped[RoleType] = mapped_column(Enum(RoleType), nullable=False)
 
 # Driver Model
 
