494c78069427abb1e2449184b169659fd9aa2138
Bla005-1 <blasupercell@gmail.com>
2026-02-14 16:20:49 -0500

    add sponsor user and org models

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,7 @@
 from app.extensions import db
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, DateTime, Enum, ForeignKey, DECIMAL
+from sqlalchemy.sql import func
 from flask_login import UserMixin
 import datetime as dt
 import enum
@@ -18,15 +19,39 @@ class User(db.Model, UserMixin):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     email: Mapped[str | None] = mapped_column(String(255), unique=True)
-    create_time: Mapped[dt.datetime] = mapped_column(DateTime, default=dt.datetime.now(dt.timezone.utc), nullable=False)
+    create_time: Mapped[dt.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     role_type: Mapped[RoleType] = mapped_column(Enum(RoleType), nullable=False)
+
 
 # Driver Model
 
-# Admin Model
 
-# Sponsor Model
+class SponsorUser(db.Model):
+    __tablename__ = 'sponsor_users'
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    sponsor_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False, unique=True)
+    organization_id: Mapped[int] = mapped_column(ForeignKey('sponsor_organization.organization_id', ondelete='RESTRICT'),nullable=False)
+    create_time: Mapped[dt.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    user = relationship('User', backref='sponsor_user', passive_deletes=True)
+    organization = relationship('SponsorOrganization', back_populates='sponsor_users', passive_deletes=True)
+
+
+class SponsorOrganization(db.Model):
+    __tablename__ = 'sponsor_organization'
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    organization_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    point_value: Mapped[float] = mapped_column(DECIMAL(10, 4), nullable=False, server_default='0.01')
+    create_time: Mapped[dt.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    sponsor_users = relationship('SponsorUser', back_populates='organization', passive_deletes=True)
